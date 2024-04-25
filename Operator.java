0170f92ecdcabd15d46b6f1294a32e9c34c021fd
manasi1406 <manasimhase2030@gmail.com>
2024-04-25 17:42:32 +0530

    Instance

--- Operator.java
+++ Operator.java
@@ -1,2 +1,18 @@
+//The total number of students in a class are 90 out of which 45 are boys. If 50% of the total students secured grade 'A' out of which 20 are boys, then write a program to calculate the total number of girls getting grade 'A'.
 public class Operator {
-}
+        public static void main(String[] args) {
+            int totalStudents = 90;
+            int totalBoys = 45;
+            int boysWithGradeA = 20;
+
+            // Calculate total students with grade 'A'
+            double totalGradeA = totalStudents * 0.5;
+
+            // Calculate total girls with grade 'A'
+            double girlsWithGradeA = totalGradeA - boysWithGradeA;
+
+            System.out.println("Total girls with grade 'A': " + (int) girlsWithGradeA);
+        }
+    }
+
+
