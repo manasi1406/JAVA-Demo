02f837c91b13231c475d20d5e774e89f995a495a
manasimhase1406 <manasimhase2030@gmail.com>
2024-05-15 07:05:43 +0530

    Multithreading

--- OnlineBankingSystem.java
+++ OnlineBankingSystem.java
@@ -1,80 +1,37 @@
-import java.util.Scanner;
-
-class BankAccount {
-    private int balance;
-
-    public BankAccount(int startingBalance) {
-        this.balance = startingBalance;
-    }
-
-    public  void deposit(int amount) {
-        balance += amount;
-        System.out.println(Thread.currentThread().getName() + " deposited " + amount + ". New balance: " + balance);
-    }
-
-    public  void withdraw(int amount) {
-        if (balance >= amount) {
-            balance -= amount;
-            System.out.println(Thread.currentThread().getName() + " withdrew " + amount + ". New balance: " + balance);
-        } else {
-            System.out.println(Thread.currentThread().getName() + " tried to withdraw " + amount + " Less Balance");
-        }
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-}
-
+//Define a class that extends Thread to represent a user action
 class UserAction extends Thread {
-     BankAccount account;
-     String action;
-      int amount;
+      String action;
 
-    public UserAction(BankAccount account, String action, int amount) {
-        this.account = account;
+    public UserAction(String action) {
         this.action = action;
-        this.amount = amount;
     }
 
 
     public void run() {
-        System.out.println("Performing action: " + action + " by " + Thread.currentThread().getName());
-        if (action.equalsIgnoreCase("deposit")) {
-            account.deposit(amount);
-        } else if (action.equalsIgnoreCase("withdraw")) {
-            account.withdraw(amount);
+        System.out.println("performing task " + action + " by " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException Ex) {
+            Ex.printStackTrace();
         }
-        System.out.println("Completed action: " + action + " by " + Thread.currentThread().getName());
+        System.out.println("Completed task: " + action + " by " + Thread.currentThread().getName());
     }
 }
 
 public class OnlineBankingSystem {
-    public static void main(String[] args) {
-        BankAccount account = new BankAccount(1000);
-        Scanner scanner = new Scanner(System.in);
+    public void main(String[] args) {
+        // Create threads for different user actions
+        Thread checkBalance = new UserAction("Check Balance");
+        Thread transferMoney = new UserAction("Transfer Money");
+        Thread payBills = new UserAction("Pay Bills");
 
-        // deposit amount
-        System.out.print("Enter deposit amount: ");
-        int depositAmount = scanner.nextInt();
+        // Start the threads
+        checkBalance.start();
+        transferMoney.start();
+        payBills.start();
 
-        //  withdrawal amounts
-        System.out.print("Enter withdrawal amount for first transaction: ");
-        int withdrawAmount1 = scanner.nextInt();
-        System.out.print("Enter withdrawal amount for second transaction: ");
-        int withdrawAmount2 = scanner.nextInt();
-
-        // threads for different user actions
-        Thread depositThread = new UserAction(account, "deposit", depositAmount);
-        Thread withdrawThread1 = new UserAction(account, "withdraw", withdrawAmount1);
-        Thread withdrawThread2 = new UserAction(account, "withdraw", withdrawAmount2);
-
-
-        depositThread.start();
-        withdrawThread1.start();
-        withdrawThread2.start();
-
-        System.out.println("Multithreading is used");
-
+        // Main thread continues its work
+        System.out.println("User is performing actions concurrently...");
     }
 }
